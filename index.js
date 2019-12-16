9b629ddaeb74f49b42a6b366a2c6dc26ac20f70d
kp1129 <katya.p.1129@gmail.com>
2019-12-16 14:44:18 -0700

    committing now while i have 6 tests passing

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  return {
+    id: id,
+    name: name,
+    email: email
+  };
 }
 
 /**
@@ -56,8 +60,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(obj) {
+  return "Hello, my name is " + obj.name;
 }
 
 /**
@@ -73,16 +77,16 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
-}
-
-
-
-
+function makeSmartPerson(name) {
+  return {
+    name: name,
+    sum: (num1, num2) => num1 + num2,
+    speak: () => `Hello, my name is ${name}`
+    }
+  }
 
 /*
-// ⭐️ Example Test Data ⭐️
+// ⭐️ Example Test Data ⭐️eeeeeee
 
 var inventory = [
   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
